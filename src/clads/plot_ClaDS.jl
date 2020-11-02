b100dc14c145b10e030ecf3c30b5b5cf654aa183
Odile Maliet <odile.maliet@orange.fr>
2020-11-02 16:42:06 +0100

    ask for R packages to be installed

--- src/clads/plot_ClaDS.jl
+++ src/clads/plot_ClaDS.jl
@@ -7,7 +7,7 @@ function create_plot_ClaDS_ape()
 
     list.of.packages <- c("ape", "fields", "RColorBrewer")
     new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
-    if(length(new.packages)){try(install.packages(new.packages, repos='http://cran.us.r-project.org'))}
+    if(length(new.packages)){print(paste0("package ", list.of.packages, " should be installed in R"))}
 	require(fields)
 	require(ape)
 	require(RColorBrewer)
